69b76a01913ad7fc23aa45fe6f5c565c9a85344c
ZeynelaErtur <zeynelabidin.ertur@ideasoft.com.tr>
2019-11-26 12:28:56 +0300

    AT-2 importlar ve ngModulleri düzenlendi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ])
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    TopBarComponent,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
